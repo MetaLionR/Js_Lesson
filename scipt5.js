ae6867140e6e83fe4b20ce69e9b00bfc6fca1cde
Mark <59655777+MetaLionR@users.noreply.github.com>
2020-03-28 16:05:29 +0500

    work_5

--- scipt5.js
+++ scipt5.js
@@ -40,19 +40,3 @@ function chessboard1() {
     document.body.appendChild( newTable );
 };
 chessboard1();
-
-
-function getObjFromNum( num ) {
-    var obj = {};
-    if ( num < 1 || num > 999 || isNaN( num )) {
-        console.log( 'Ошибка. Введите число от 1 до 999' );
-        return obj;
-    }
-    else {
-        obj[ 'едининцы' ] = num % 10;
-        obj[ 'десятки' ] = ( num - obj[ 'едининцы' ]) % 100 / 10;
-        obj[ 'сотни' ] = ( num - obj[ 'едининцы' ] - obj[ 'десятки' ] * 10 ) % 1000 / 100;
-        return obj; 
-    }
-}
-console.log( getObjFromNum( 999 ));
